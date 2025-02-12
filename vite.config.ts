b3725d68e2fa003c0f6b557ea8a2da90422829bf
bvoo <bvoowo@gmail.com>
2025-02-11 23:54:22 -0800

    feat: pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,65 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      injectRegister: 'auto',
+      manifest: {
+        name: 'remapp.ing',
+        short_name: 'remapp.ing',
+        description: 'An open source Haybox remapping tool',
+        theme_color: '#ebebeb',
+        icons: [
+          {
+            src: '/icons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png',
+            purpose: 'apple touch icon'
+          },
+          {
+            src: '/icons/favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/favicon-16x16.png',
+            sizes: '16x16',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/favicon.ico',
+            sizes: '48x48',
+            type: 'image/x-icon'
+          }
+        ],
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ebebeb'
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,ttf}']
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
